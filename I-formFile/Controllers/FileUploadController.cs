f44969833274db8af16bdc742f58f8cd94f8e525
Shubh <shubhw6@gmail.com>
2023-09-18 12:12:59 +0530

    Add project files.

---
+++ I-formFile/Controllers/FileUploadController.cs
@@ -0,0 +1,39 @@
+﻿using I_formFile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace I_formFile.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            SingleFileUpload model = new SingleFileUpload();
+
+            return View(model);
+        }
+
+
+        public IActionResult SingleFileSave(SingleFileUpload model)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/FileSave");
+            //create folder if not exist
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+
+            }
+            //get file extension
+            model.FileName = Guid.NewGuid().ToString()+model.File.FileName;
+
+            string fileNameWaithPath=Path.Combine(path,model.FileName);
+
+            using (var stream = new FileStream(fileNameWaithPath, FileMode.Create))
+            {
+                model.File.CopyTo(stream);
+            }
+            model.IsSuccess=true;
+            model.Message = "File Uploaded SuccessFully !";
+                return View("Index",model);
+        }
+    }
+}
---
+++ I-formFile/Models/ResopnseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace I_formFile.Models
+{
+    public class ResopnseModel
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsResponse { get; set; }
+
+
+    }
+
+
+    public class SingleFileUpload: ResopnseModel
+    {
+        public string FileName { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
